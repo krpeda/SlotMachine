e0dfe0f9c6d5b0d752d81def0fea87139bf563ab
Kristjan Erik Pedak <kristjan.pedak@gmail.com>
2018-05-20 12:37:48 +0300

    Button functionality

--- index.js
+++ index.js
@@ -66,37 +66,61 @@
     for (let i = 0; i < stageArray.length; i++) {
         createTextures(symbolArray, stageArray[i]);     
     }
-    
+    requestAnimationFrame(update);
  }
  function update() {
-    
-    
+
     slotOneRenderer.render(stageOne);
     slotTwoRenderer.render(stageTwo);
     slotThreeRenderer.render(stageThree);
     slotFourRenderer.render(stageFour);
     requestAnimationFrame(update);
  }
+ 
  function createTextures(array, stage) {
-    var spriteArray = [];
-     for (let i = 0; i < 12; i++) {
+     for (let i = 0; i < 42; i++) {
         var element = array[Math.floor(Math.random() * array.length)]
         var symbol = PIXI.Texture.fromImage("assets/images/" + element.id);
-        sprite = new PIXI.extras.TilingSprite(symbol, 400, 336);
+        sprite = new PIXI.Sprite(symbol);
         sprite.position.y = Math.floor(i / 5) * 50;
         sprite.scale.x = 0.75;
-        sprite.scale.y = 0.15;    
-        sprite.tilePosition.y = 0;     
+        sprite.scale.y = 0.15;      
         stage.addChild(sprite);   
         requestAnimationFrame(update);
-        spriteArray.push(sprite);
      };
-     console.log(spriteArray.length);
+     console.log(stage.children);  //amount of symbols 
  }
  
  function spin() {
+
+    var userBalance = document.getElementById("userBalance");
+    var balanceValue = parseInt(userBalance.getAttribute("value"), 10);
+    var betValue = parseInt(totalBet.getAttribute("value"), 10);
+    if (balanceValue >= betValue) { 
+    var newValue = balanceValue - betValue;
+    userBalance.setAttribute("value", newValue);
+    userBalance.innerHTML = newValue;
+
     stageOne.position.y -= 1;
     stageTwo.position.y -= 0.2;
     stageThree.position.y -= 0.09;
     stageThree.position.y -= 0.11;
+    requestAnimationFrame(update);
+    }   
+}
+function increase() {
+        var totalBet = document.getElementById("totalBet");
+        var newValue = parseInt(totalBet.getAttribute("value"), 10) + 10;
+        totalBet.setAttribute("value", newValue);
+        totalBet.innerHTML = newValue;
+}
+function decrease() {
+    var totalBet = document.getElementById("totalBet");
+    var value = parseInt(totalBet.getAttribute("value"), 10);
+    if (value >= 10) {  
+        var newValue = value - 10;
+        totalBet.setAttribute("value", newValue);
+        totalBet.innerHTML = newValue;
+    }
+    
 }
